0f30a3943ac92cb5ffe773a34e34308b87c3b3d0
Jesus Sanz <jbravosanz@gmail.com>
2021-05-12 15:04:20 +0200

    Add files via upload

---
+++ EjemploDelegados/EjemploDelegados/AsistenteDescarga.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EjemploDelegados
+{
+    class AsistenteDescarga
+    {
+        //paso 1 crear delegado
+        public delegate void GestorEventoArchivoDescargado(object fuente, EventArgs args);
+
+        //Paso 2 crear el evento basado en el delegado
+        public event GestorEventoArchivoDescargado ArchivoDescargado;
+
+        //Paso 3 inciar el evento
+        protected virtual void EnArchivoDescargado()
+        {
+            if (ArchivoDescargado != null)
+            {
+                ArchivoDescargado(this, EventArgs.Empty);
+            }
+        }
+
+        public void Descarga(Archivo archivo)
+        {
+            Console.WriteLine("Descargando...");
+            Thread.Sleep(4000);
+
+            //Paso 3.1
+            EnArchivoDescargado();
+        }
+    }
+}
